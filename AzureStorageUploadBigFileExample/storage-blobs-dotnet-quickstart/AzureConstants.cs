9380482a61ee70591fdd19d81a8cabc147468dff
Guan Jun <ty6335409@qq.com>
2019-05-07 17:03:38 +0800

    final commit
    final commit

---
+++ AzureStorageUploadBigFileExample/storage-blobs-dotnet-quickstart/AzureConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureStorageUploadBigFileExample
+{
+	internal class AzureConstants
+	{
+		//存储账户
+		public static string Account = "guanjunstorage";
+
+		//存储密码
+		public static string SecretKey = "IORRlrxprU0p8gSBaXzPzaTAoeu722Nvp6sjckdz1s3/6DmcFdQDmsOmbRtyW/GpoETWNvBfd50/C6Jl5oRXyg==";
+
+		public static string SharedKeyAuthorizationScheme = "SharedKey";
+	}
+}
